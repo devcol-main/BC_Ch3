4a7a93c9eb82155872550e294677c5cb376cb453
DevCol <devcol.main@gmail.com>
2026-06-05 11:37:44 +0900

    Initial commit

---
+++ Source/BC_Ch3/BC_Ch3.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BC_Ch3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BC_Ch3, "BC_Ch3" );
